3f5ee48ca30c7b4a75ccbc8e23eaa9c1fb2af834
tines98 <tinesvalen@gmail.com>
2026-02-16 19:10:24 +0100

    update stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*gitui*",
+  callback = function()
+    vim.cmd([[
+      hi Terminal guibg=#1a1b26 guifg=#c0caf5
+      hi TermCursor guibg=#7aa2f7 guifg=#1a1b26
+    ]])
+  end,
+})
