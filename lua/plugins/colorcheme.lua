9d00061b84617b3b0aa7a55f1135761bec19f075
tines98 <tinesvalen@gmail.com>
2026-03-18 14:08:55 +0100

    changes selection color in colorscheme

--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "sainnhe/gruvbox-material",
+    config = function()
+      -- gruvbox-material specific settings must come BEFORE colorscheme is set
+      vim.g.gruvbox_material_visual = "reverse" -- or "grey background", "blue background", etc.
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-material", -- or "kanagawa-wave", "everforest", etc.
