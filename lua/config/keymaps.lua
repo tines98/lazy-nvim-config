185a4c17f59232b2cd1f4adb4cf8fcb33a64d3bd
tines98 <tinesvalen@gmail.com>
2026-03-18 14:07:57 +0100

    remaps [ ] keys to norwegian å and ¨

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,10 @@ local map = vim.keymap.set
 -- Move text up and down
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+-- Norwegian keyboard: map å and ¨ to [ and ] namespaces
+vim.keymap.set("n", "å", "[", { remap = true })
+vim.keymap.set("n", "¨", "]", { remap = true })
+vim.keymap.set("o", "å", "[", { remap = true })
+vim.keymap.set("o", "¨", "]", { remap = true })
+vim.keymap.set("x", "å", "[", { remap = true })
+vim.keymap.set("x", "¨", "]", { remap = true })
