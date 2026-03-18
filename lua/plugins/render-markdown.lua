7c04b6c24b86201d3c1d39857bb98117cd22a53a
tines98 <tinesvalen@gmail.com>
2026-03-18 14:11:17 +0100

    adds config for render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown", "copilot-chat" },
+  opts = {
+    heading = {
+      sign = true,
+      icons = { "", "", "", "", "", "" },
+    },
+    render_modes = true,
+  },
+}
