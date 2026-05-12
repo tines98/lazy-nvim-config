863cb2c04bf89679b489b3194336552cd49aa97f
tines98 <tinesvalen@gmail.com>
2026-05-12 14:24:32 +0200

    fixed next prev keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,13 +6,13 @@ local map = vim.keymap.set
 -- Move text up and down
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
--- Norwegian keyboard: map å and ¨ to [ and ] namespaces
+-- Norwegian keyboard: map å and æ to [ and ] namespaces
 vim.keymap.set("n", "å", "[", { remap = true })
-vim.keymap.set("n", "¨", "]", { remap = true })
+vim.keymap.set("n", "æ", "]", { remap = true })
 vim.keymap.set("o", "å", "[", { remap = true })
-vim.keymap.set("o", "¨", "]", { remap = true })
+vim.keymap.set("o", "æ", "]", { remap = true })
 vim.keymap.set("x", "å", "[", { remap = true })
-vim.keymap.set("x", "¨", "]", { remap = true })
+vim.keymap.set("x", "æ", "]", { remap = true })
 
 -- Open iTerm2 vertical split in current project directory
 map("n", "<leader>tv", function()
@@ -24,8 +24,3 @@ map("n", "<leader>th", function()
   local cwd = vim.fn.getcwd()
   vim.fn.system("ttab -h -i -d " .. cwd)
 end, { desc = "iTerm2 horizontal split (cwd)" })
-
-map("n", "<leader>av", function()
-  local cwd = vim.fn.getcwd()
-  vim.fn.system("ttab -v -i -d " .. cwd .. " opencode .")
-end, { desc = "Open Opencode in split tab (cwd)" })
